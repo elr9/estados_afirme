e821c3d996f3f7c70e96fd17888de6181298f6cb
elr9 <90631728+elr9@users.noreply.github.com>
2023-12-15 14:18:32 -0600

    Update estados_afirme.py

--- estados_afirme.py
+++ estados_afirme.py
@@ -35,9 +35,9 @@ def to_excel(df):
     output = BytesIO()
     with pd.ExcelWriter(output, engine='xlsxwriter') as writer:
         df.to_excel(writer, index=False, sheet_name='Sheet1')
-        writer.save()
     processed_data = output.getvalue()
     return processed_data
+
 
 # Streamlit application layout
 st.title('Bank Statement Processor')
