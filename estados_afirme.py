a9c0c8106dd5575db7b2cc4bdc6bd6474d5bec94
elr9 <90631728+elr9@users.noreply.github.com>
2023-12-15 14:49:41 -0600

    Update estados_afirme.py

--- estados_afirme.py
+++ estados_afirme.py
@@ -3,8 +3,8 @@ import pandas as pd
 import base64
 from io import BytesIO
 
-# Function to process the Excel file
-def process_bank_statement(uploaded_file):
+# Function to process the Afirme bank statement
+def process_afirme_statement(uploaded_file):
     # Read the Excel file
     bank_data = pd.read_excel(uploaded_file, header=7)
 
@@ -30,6 +30,30 @@ def process_bank_statement(uploaded_file):
 
     return bank_data, abono_sum
 
+# Function to process the Hey bank statement
+def process_hey_statement(uploaded_file):
+    # Read the CSV file
+    bank_data = pd.read_csv(uploaded_file, skiprows=9)
+
+    # Rename columns for consistency
+    bank_data.columns = ['Fecha', 'Descripción', 'Referencia', 'Cargo', 'Abonos', 'Saldo', 'Clasificación']
+
+    # Convert 'Abonos' to numeric values
+    bank_data['Abonos'] = pd.to_numeric(bank_data['Abonos'].str.replace('$', '').str.replace(',', ''), errors='coerce')
+
+    # Initialize 'Comentarios' and 'Considerar' columns
+    bank_data['Comentarios'] = ''
+    bank_data['Considerar'] = 'si' # Default to 'si'
+
+    # Apply rules for 'Comentarios' and 'Considerar'
+    bank_data.loc[bank_data['Descripción'].str.contains('TARJETA DE CREDITO B|propias|Ahorro', case=False, na=False), ['Comentarios', 'Considerar']] = ['Traspaso entre cuentas propias', 'no']
+    bank_data.loc[bank_data['Descripción'].str.contains('recompensas', case=False, na=False), ['Comentarios', 'Considerar']] = ['recompensas', 'no']
+
+    # Calculate the sum of 'Abonos' where 'Considerar' is 'si'
+    abonos_sum = bank_data[bank_data['Considerar'] == 'si']['Abonos'].sum()
+
+    return bank_data, abonos_sum
+
 # Function to download data as Excel
 def to_excel(df):
     output = BytesIO()
@@ -38,23 +62,37 @@ def to_excel(df):
     processed_data = output.getvalue()
     return processed_data
 
-
 # Streamlit application layout
 st.title('Bank Statement Processor')
 
-# File uploader
-uploaded_file = st.file_uploader("Choose a file", type=['xlsx'])
+# File uploader for Afirme
+st.subheader('Afirme Bank Statement')
+uploaded_file_afirme = st.file_uploader("Choose an Afirme file", type=['xlsx'], key='afirme')
 
-if uploaded_file is not None:
-    # Process the file
-    cleaned_data, total_abono = process_bank_statement(uploaded_file)
+# File uploader for Hey
+st.subheader('Hey Bank Statement')
+uploaded_file_hey = st.file_uploader("Choose a Hey file", type=['csv'], key='hey')
 
-    # Display results
-    st.write(f"Total Sum of 'Abono': ${total_abono:,.2f}")
+# Process Afirme file
+if uploaded_file_afirme is not None:
+    cleaned_data_afirme, total_abono_afirme = process_afirme_statement(uploaded_file_afirme)
+    st.write(f"Total Income from Afirme: ${total_abono_afirme:,.2f}")
 
-    # Download link for cleaned data
-    if st.button('Download Cleaned Data as Excel'):
-        processed_data = to_excel(cleaned_data)
-        b64 = base64.b64encode(processed_data).decode()
-        href = f'<a href="data:application/octet-stream;base64,{b64}" download="cleaned_data.xlsx">Download Excel File</a>'
-        st.markdown(href, unsafe_allow_html=True)
+    # Download link for Afirme data
+    if st.button('Download Afirme Data as Excel', key='download_afirme'):
+        processed_data_afirme = to_excel(cleaned_data_afirme)
+        b64_afirme = base64.b64encode(processed_data_afirme).decode()
+        href_afirme = f'<a href="data:application/octet-stream;base64,{b64_afirme}" download="afirme_data.xlsx">Download Afirme Excel File</a>'
+        st.markdown(href_afirme, unsafe_allow_html=True)
+
+# Process Hey file
+if uploaded_file_hey is not None:
+    cleaned_data_hey, total_abonos_hey = process_hey_statement(uploaded_file_hey)
+    st.write(f"Total Income from Hey: ${total_abonos_hey:,.2f}")
+
+    # Download link for Hey data
+    if st.button('Download Hey Data as Excel', key='download_hey'):
+        processed_data_hey = to_excel(cleaned_data_hey)
+        b64_hey = base64.b64encode(processed_data_hey).decode()
+        href_hey = f'<a href="data:application/octet-stream;base64,{b64_hey}" download="hey_data.xlsx">Download Hey Excel File</a>'
+        st.markdown(href_hey, unsafe_allow_html=True)
