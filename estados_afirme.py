c53430da29f0a23daf4fe2033cb700bad27c2696
elr9 <90631728+elr9@users.noreply.github.com>
2023-12-15 14:15:43 -0600

    Update estados_afirme.py

--- estados_afirme.py
+++ estados_afirme.py
@@ -33,9 +33,9 @@ def process_bank_statement(uploaded_file):
 # Function to download data as Excel
 def to_excel(df):
     output = BytesIO()
-    writer = pd.ExcelWriter(output, engine='xlsxwriter')
-    df.to_excel(writer, index=False, sheet_name='Sheet1')
-    writer.save()
+    with pd.ExcelWriter(output, engine='xlsxwriter') as writer:
+        df.to_excel(writer, index=False, sheet_name='Sheet1')
+        writer.save()
     processed_data = output.getvalue()
     return processed_data
 
